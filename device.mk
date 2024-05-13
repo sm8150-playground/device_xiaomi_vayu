f643a9c456405cae273ecb52bf5e5d5d0dea199b
TheScarastic <warabhishek@gmail.com>
2024-05-14 00:54:05 +0530

    vayu: Add thermal and refresh rate settings
    Change-Id: I8a81c34f9469cc593a6c7019996150794d0ddee5
    co-authored-by: someone5678 <nemui3353@gmail.com>
    co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    co-authored-by: Lostark13 <tushar9915@gmail.com>
    co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -138,6 +138,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
