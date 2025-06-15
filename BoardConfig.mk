288181cbda6e8dab9618129ba0ead7f486cd67cf
grepfox <grepfox@tutamail.com>
2025-06-15 15:39:33 +0530

    vayu: Update from  miui_NABUINGlobal_OS1.0.18.0
    Change-Id: I520f190c2fa2c81ccec2fd900370e0f6438a51e9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,7 +136,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-03-01
+VENDOR_SECURITY_PATCH := 2025-04-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
