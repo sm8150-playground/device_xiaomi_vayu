cf65a502d6a3cae2138c89cb376840263f91a496
grepfox <grepfox@tutamail.com>
2025-07-15 15:04:46 +0530

    vayu: Move *.prop to configs/props
    * remove unused product.prop while at it
    
    Change-Id: Ibbb0da701f145a29598c0c4fd3a7684e76536bff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,9 +110,9 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := msmnile
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
