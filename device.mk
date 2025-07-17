bec3ed04afc681c47df419ea750725fa0e4861d9
grepfox <grepfox@tutamail.com>
2025-07-18 01:06:56 +0530

    vayu: LineageEra
    This reverts commit 82d07e33b6dd861906e2244c60a5633ee7ea797c.
    
    Change-Id: I1d17976d1e8ca4af6f17f46aaf4c8d07c93d7594

--- device.mk
+++ device.mk
@@ -247,7 +247,8 @@ PRODUCT_PACKAGES += \
     ApertureOverlayVayu \
     CarrierConfigOverlayVayu \
     FrameworkResOverlayVayu \
-    YAAPSettingsOverlayDevice \
+    LineageDialerOverlayVayu \
+    LineageSDKOverlayVayu \
     SettingsOverlayVayu \
     SettingsProviderOverlayVayu \
     SystemUIOverlayVayu \
@@ -255,9 +256,6 @@ PRODUCT_PACKAGES += \
     WifiResourcesOverlayVayu
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-
-# Platform
-TARGET_BOARD_PLATFORM := msmnile
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -268,8 +266,6 @@ PRODUCT_PACKAGES += \
     vendor_firmware_mnt_mountpoint
 
 # Power
-TARGET_PROVIDES_POWERHAL := true
-
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
@@ -337,6 +333,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
+
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # UFFD GC
 OVERRIDE_ENABLE_UFFD_GC := false
