e1ca2a4e808a35aadbb735a071cc7f363e0b424f
grepfox <grepfox@tutamail.com>
2025-07-18 01:12:26 +0530

    vayu: axion setup
    Change-Id: I759d8e1731c4fd970a4aa1a91ed9894a99315442

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -14,6 +14,32 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
+
+# Axion flags
+AXION_CAMERA_REAR_INFO := 48,8,5,2
+AXION_CAMERA_FRONT_INFO := 20
+AXION_MAINTAINER := grepfox
+AXION_PROCESSOR := Snapdragon_860
+BYPASS_CHARGE_SUPPORTED := false
+AXION_CPU_SMALL_CORES ?= 4,5,6,7
+AXION_CPU_BIG_CORES ?= 0,1,2,3
+AXION_CPU_BG ?= 0-2
+AXION_CPU_FG ?= 0-5
+AXION_CPU_LIMIT_BG ?= 0-1
+AXION_DEBUGGING_ENABLED := false
+TARGET_INCLUDE_VIPERFX := false
+TARGET_ENABLE_BLUR := false
+TARGET_INCLUDES_LOS_PREBUILTS := true
+
+# AxionOS scheduling properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.axion_cpu_big=$(AXION_CPU_BIG_CORES) \
+    persist.sys.axion_cpu_small=$(AXION_CPU_SMALL_CORES) \
+    persist.sys.axion_cpu_bg=$(AXION_CPU_BG) \
+    persist.sys.axion_cpu_limit_bg=$(AXION_CPU_LIMIT_BG) \
+    persist.sys.axion_cpu_fg=$(AXION_CPU_FG)
+    ro.sys.axion_userdebug_enabled=$(AXION_DEBUGGING_ENABLED)
+
 PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
