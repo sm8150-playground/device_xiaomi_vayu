1b4519d96f842d8ec71c54b3c61a7eb4c31f2ef4
grepfox <grepfox@tutamail.com>
2025-07-26 15:11:12 +0530

    vayu: welcome axion
    Change-Id: I0f1681b7f91a1fb01bd8c841bbd9a4c0eee9f5fb

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2021 The LineageOS Project
-# Copyright (C) Yet Another AOSP Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,13 +8,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-PRODUCT_NAME := yaap_vayu
+# Axion flags
+AXION_CAMERA_REAR_INFO := 48,8,5,2
+AXION_CAMERA_FRONT_INFO := 20
+AXION_MAINTAINER := grepfox
+AXION_PROCESSOR := Snapdragon_860
+BYPASS_CHARGE_SUPPORTED := false
+AXION_DEBUGGING_ENABLED := false
+TARGET_INCLUDE_VIPERFX := false
+TARGET_ENABLE_BLUR := false
+TARGET_INCLUDES_LOS_PREBUILTS := true
+
+# AxionOS properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.sys.axion_userdebug_enabled=$(AXION_DEBUGGING_ENABLED)
+
+PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
