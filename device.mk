988ffb9eac795043795193cd6ac999a2e9185175
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    vayu: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- device.mk
+++ device.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
     libvisualizer \
     libvolumelistener
 
-$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # Audio configs
 PRODUCT_COPY_FILES += \
