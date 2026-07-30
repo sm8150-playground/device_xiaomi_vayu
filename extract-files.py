25afc4f825f9956d6dcd6d6e971564901fd07ca6
grep <grepfox@tutamail.com>
2026-07-30 12:59:07 +0000

    [SQUASH] vayu: Revert to AOSP WFD
    Revert "vayu: Update WFD (system) blobs from lexus"
    
    This reverts commit f4d06e22039b2164ae5710f063afebe66b0eac33.
    
    Revert "vayu: Move WfdCommon boot jar declaration to vendor"
    
    This reverts commit 25a8e803a8cdf14a29f2a7fcb2b185001ca72a14.
    
    Revert "vayu: Update WFD (vendor) from LA.UM.9.1.r1-13700-SMxxx0.QSSI13.0"
    
    This reverts commit bbf379c3d5613700fd31fe1d4ae78c61c4f3e095.
    
    Revert "vayu: Restore QCOM WFD"
    
    This reverts commit fce262efd02bf8afbd4874e7c8acd7ee7d2bb595.

--- extract-files.py
+++ extract-files.py
@@ -43,7 +43,6 @@ lib_fixups: lib_fixups_user_type = {
         'com.qualcomm.qti.dpm.api@1.0',
         'libmmosal',
         'vendor.qti.hardware.fm@1.0',
-        'vendor.qti.hardware.wifidisplaysession@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
 }
@@ -73,8 +72,6 @@ blob_fixups: blob_fixups_user_type = {
             b'\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63',
             b'\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74',
         ),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .add_needed('libinput_shim.so'),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
         .regex_replace(r'seclabel u:r:mi_thermald:s0', ''),
     'vendor/lib64/libdpps.so': blob_fixup()
